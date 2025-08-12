6cd4e799c99ac4360855a86afbded9f1fcbd55a9
vijaymbaphd <vijaymbaphd@gmail.com>
2025-08-12 23:21:36 +0530

    Automated update via R script

--- install.R
+++ install.R
@@ -1,7 +1,4 @@
 # install.R
-# Packages to install when Binder / repo2docker builds the environment
-install.packages('exact2x2', repos = 'https://cloud.r-project.org')
-install.packages("exact2x2", repos = 'https://cloud.r-project.org')
-install.packages("ggplot2", repos = 'https://cloud.r-project.org')
-
+install.packages("exact2x2")
+install.packages("ggplot2")
 # Add any other packages your book uses
